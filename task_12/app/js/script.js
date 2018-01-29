a9a19c7d2c7f1698e632b7986dc1449d65a5a58c
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-01-29 08:17:11 +0200

    setInterval for clock

--- task_12/app/js/script.js
+++ task_12/app/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function getTime() {
+	const now = new Date();
+	let ss = now.getSeconds();
+		if (ss < 10) ss = '0' + ss;
+	let mm = now.getMinutes();
+		if (mm < 10) mm = '0' + mm;
+	let hh = now.getHours();
+		if (hh < 10) hh = '0' + hh;
+
+	return `${hh}:${mm}:${ss}`;
+}
+
+const clock = document.querySelector('#current-time');
+
+setInterval( () => {
+	clock.innerHTML = getTime();
+}, 1000);
+
