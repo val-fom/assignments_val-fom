333c0c9bafd2e3abc2155e88b3eaaa05f3617503
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-03 05:33:28 +0200

    setup webpack.config.js and add appropriate packages

---
+++ task_12/webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var inProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+
+	entry: {
+		app: [
+			'./src/main.js',
+			'./src/main.scss'
+		]
+	},
+
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].js'
+	},
+
+	module: {
+		rules: [
+
+			{
+				test: /\.s[ac]ss$/,
+				use: ExtractTextPlugin.extract({
+					use: ['css-loader', 'sass-loader'],
+					fallback: 'style-loader'
+				})
+			},
+
+			{ 
+				test: /\.js$/, 
+				exclude: /node_modules/, 
+				loader: "babel-loader"
+			}
+
+		]
+	},
+
+	plugins: [
+
+		new ExtractTextPlugin("[name].css"),
+
+		new webpack.LoaderOptionsPlugin({
+			minimize: inProduction
+		})
+
+	]
+};
+
+if (inProduction) {
+	module.exports.plugins.push(
+			new webpack.optimize.UglifyJsPlugin()
+		)
+}
+
