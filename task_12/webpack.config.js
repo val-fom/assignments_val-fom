a7b6a2964d6e24392513a9d3d1f61a683bcbe2ad
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-02-03 22:19:58 +0200

    implement font-awesome 4.7 from package

--- task_12/webpack.config.js
+++ task_12/webpack.config.js
@@ -44,6 +44,11 @@ module.exports = {
 			})
 		},
 
+		{
+			test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+			loader: 'file-loader?name=fonts/[name].[ext]'
+		},
+
 		{ 
 			test: /\.js$/, 
 			exclude: /node_modules/, 
